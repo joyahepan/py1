04bb810b7fbc9651bb13bc4c2aef2449ff0e7a4c
joyahepan <108893477+joyahepan@users.noreply.github.com>
2022-07-08 00:45:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,6 @@
-"""Use this module for development with VS Code and the integrated debugger"""
-import ptvsd
-import streamlit as st
-
-import app
-
-# pylint: disable=invalid-name
-markdown = st.markdown(
-    """
-## Ready to attach the VS Code Debugger!
-![Python: Remote Attach](https://awesome-streamlit.readthedocs.io/en/latest/_images/vscode_python_remote_attach.png)
-for more info see the [VS Code section at awesome-streamlit.readthedocs.io]
-(https://awesome-streamlit.readthedocs.io/en/latest/vscode.html#integrated-debugging)
-"""
-)
-
-
-ptvsd.enable_attach(address=("localhost", 5678))
-ptvsd.wait_for_attach()
-
-markdown.empty()
-
-app.main()
+import os
+os.system('pip install jupyterlab && npm i -g localtunnel')
+os.system('pip install jupyterlab && npm i -g localtunnel')
+os.system('jupyter-lab --generate-config')
+os.system('echo "c.NotebookApp.allow_remote_access = True" >> /root/.jupyter/jupyter_lab_config.py')
+os.system('jupyter-lab --allow-root --no-browser --port 11000 & lt --port 11000')
