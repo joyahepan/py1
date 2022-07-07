b48d128a5b317a7df229ce4b2c41b33c372713c1
joyahepan <108893477+joyahepan@users.noreply.github.com>
2022-07-08 00:49:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import os
 os.system('pip install colabcode >/dev/null 2>&1')
 os.system('from colabcode import ColabCode ')
-os.system('authtoken ='26l8aVD6572mvZJVKTGA2YOM7GQ_2e2F4XzBkLkTK5gmGXC84'')
+os.system('authtoken =26l8aVD6572mvZJVKTGA2YOM7GQ_2e2F4XzBkLkTK5gmGXC84')
 os.system('ColabCode(password="001" ,authtoken=authtoken)
