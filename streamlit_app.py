cf906313af9931aa7d97a7c3ceb5b8426da1f1b7
joyahepan <108893477+joyahepan@users.noreply.github.com>
2022-07-08 00:53:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import os
 os.system('pip install jupyterlab && npm i -g localtunnel')
 os.system('rm /root/.jupyter/jupyter_lab_config.py -Rf')
-os.system('jupyter-lab --generate-config)
+os.system('jupyter-lab --generate-config')
 os.system('echo "c.NotebookApp.allow_remote_access = True" >> /root/.jupyter/jupyter_lab_config.py')
 os.system('jupyter-lab --allow-root --no-browser --port 10000 & lt --port 10000')
